7a3d1b746b112062b388245557b5e94687c55685
ahmedlabib1729 <a.labib1707@gmail.com>
2024-12-18 08:47:43 +0000

    Initial commit

--- product_dimensions/models/product_template_dimensions.py
+++ product_dimensions/models/product_template_dimensions.py
@@ -3,9 +3,21 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    length_cm = fields.Float(string="Length (cm)", help="Length of the product in centimeters")
-    width_cm = fields.Float(string="Width (cm)", help="Width of the product in centimeters")
-    height_cm = fields.Float(string="Height (cm)", help="Height of the product in centimeters")
+    length_cm = fields.Float(
+        string="Length (cm)",
+        help="Length of the product in centimeters",
+        digits=(16, 10)  # دقة 10 خانات بعد العلامة العشرية
+    )
+    width_cm = fields.Float(
+        string="Width (cm)",
+        help="Width of the product in centimeters",
+        digits=(16, 10)
+    )
+    height_cm = fields.Float(
+        string="Height (cm)",
+        help="Height of the product in centimeters",
+        digits=(16, 10)
+    )
 
     @api.depends('length_cm', 'width_cm', 'height_cm')
     def _compute_volume(self):
